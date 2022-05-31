a9c2283778c5d24f8257966fea8209df83de098d
sasanek12 <sasanek12@gmail.com>
2022-05-31 22:35:54 +0200

    a

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-import string
 import sys
-import requests
 import time
-from bs4 import BeautifulSoup
-from typing import List
+
+import requests
 
 
 def main():
-    if()
+    if (sys.argv.__len__() != 2):
+        print("Program otrzymał nieprawidłową liczbę argumentów")
+        sys.exit(1)
     url = "https://api.discogs.com/artists/"
     url_band = url + sys.argv[1]
     response = requests.get(url_band)
+    while (response.status_code != 200):
+        response = requests.get(url_band)
+        if (response.status_code == 429):
+            print("oczekiwane na reset timeouta")
+            time.sleep(5)
+        else:
+            print("Błąd API, komunikat błędu:", response)
+            sys.exit(1)
     data = response.json()
-    members_database = []
     groups = {}
     band_members = []
     for member in data['members']:
@@ -28,9 +34,13 @@ def main():
             url_member = url + str(member['id'])
             response_member = requests.get(url_member)
 
-            if(response_member.status_code != 200):
-                print("oczekiwane na reset timeouta")
-                time.sleep(5)
+            if (response_member.status_code != 200):
+                if (response_member.status_code == 429):
+                    print("oczekiwane na reset timeouta")
+                    time.sleep(5)
+                else:
+                    print("Błąd API, komunikat błędu:", response)
+                    sys.exit(1)
             else:
                 # print(response_member.json())
                 # print(response_member.status_code)
@@ -48,15 +58,15 @@ def main():
     sorted_band_members = sorted(band_members)
     for band in sorted_dict:
         sorted_dict[band] = sorted(sorted_dict[band])
-    print("Wybrany Zespół -",data['name'],":",','.join(sorted_band_members))
+    print("Wybrany Zespół -", data['name'], ":", ', '.join(sorted_band_members))
     print("Zespoły w których występuje dwoje lub więcej członków wybranego zespołu: ")
     for i in sorted_dict:
         if sorted_dict[i].__len__() >= 2:
-            print(i,": ", ', '.join(sorted_dict[i]))
-
+            print(i, ": ", ', '.join(sorted_dict[i]))
 
     # print(data['members'])
     # print(result)
+
 
 if __name__ == "__main__":
     main()
